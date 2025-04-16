041c30f9c0b036cb5b284672d775366ec663c13f
Anders Lie <anders.erik.lie@gmail.com>
2025-04-16 18:41:12 -0400

    wip

--- packages/magnitude-core/src/common/failure.ts
+++ packages/magnitude-core/src/common/failure.ts
@@ -1,7 +1,52 @@
 // TODO: flesh out
+/**
+ * Represents any reason why a test case could have failed, for example:
+ * - Step could not be completed
+ * - Check did not pass
+ * - Could not navigate to starting URL
+ * - Time or action based timeout
+ * - ...
+ */
 export interface FailureDescriptor {
-    // could represent "code" errors i.e. failed to connect to page,
-    // or actual problems with the site. should have appropriate fields to describe precisely the failure.
-    // todo: severity etc.
     description: string
+}
+
+export type BugSeverity = 'critical' | 'high' | 'medium' | 'low';
+
+export interface BugClassification {
+    title: string,
+    expectedResult: string,
+    actualResult: string,
+    severity: BugSeverity
+}
+
+export interface StepFailure {
+    /**
+     * A step fails when agent cannot find a way to complete it.
+     * When can this happen?
+     * (A) There's a bug that prevents in the step from being completed
+     * (B) The step doesn't align at all with how the interface currently works
+     *     (i.e. the interface changed completely or a step was written incorrectly)
+     */
+    variant: 'step',
+    bug: BugClassification
+}
+
+export interface CheckFailure {
+    /**
+     * A check fails when it is evaluated and does not hold true.
+     * When can this happen?
+     * (A) There's a bug that causes the check to fail
+     * (B) The check was written in a way that will always fail / doesn't align with interface
+     */
+    variant: 'check'
+    bug: BugClassification
+}
+
+
+export interface UnknownFailure {
+    // Failure due to some unknown / unhandled error.
+    // If these are being returned we should identify and handle them specifically
+    variant: 'unknown',
+
 }
