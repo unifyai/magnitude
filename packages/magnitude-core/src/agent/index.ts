2d600f5585458c89123ba1596b70c021bae8b076
YushaArif99 <yushaa799@gmail.com>
2025-11-10 12:27:22 +0500

    increase query limit for cache candidates from 3 to 10

--- packages/magnitude-core/src/agent/index.ts
+++ packages/magnitude-core/src/agent/index.ts
@@ -813,7 +813,7 @@ export class Agent {
                 project: project,
                 context: context,
                 filter_expr: filterClauses.join(' and '),
-                limit: 3,
+                limit: 10,
                 sorting: JSON.stringify(sortingObject)
             };
             
